b9604c8d3aa3752936b96819558fed12778306bc
MaxSmile44 <afmaxim89@gmail.com>
2025-02-04 14:00:29 +0300

    Add lesson3

---
+++ scripts.py
@@ -0,0 +1,117 @@
+from random import choice
+
+from datacenter.models import Chastisement
+from datacenter.models import Commendation
+from datacenter.models import Lesson
+from datacenter.models import Mark
+from datacenter.models import Schoolkid
+
+
+def fix_marks(child):
+    """Изменить оценки с двоек и троек на пятерки.
+
+    Args:
+        child (str): ФИО ученика.
+
+    Returns:
+        На сайте меняются оценки.
+        Если количество найденных учеников отличается от одного, то выходит соответствующее уведомление.
+    """
+    try:
+        child = Schoolkid.objects.get(full_name__contains = child)
+    except Schoolkid.DoesNotExist:
+        print('Записей не найдено')
+        return
+    except Schoolkid.MultipleObjectsReturned:
+        print('Найдено больше одной записи')
+        return
+    for _ in range(0, Mark.objects.filter(schoolkid=child, points__in=[2, 3]).count()):
+        new_point = Mark.objects.filter(schoolkid = child, points__in=[2,3]).first()
+        new_point.points = 5
+        new_point.save()
+    return
+
+
+def remove_chastisements(child):
+    """Удалить записи с замечанием от учителя ученику.
+
+    Args:
+        child (str): ФИО ученика.
+
+    Returns:
+        На сайте удаляются все записи с замечаниями ученика.
+        Если количество найденных учеников отличается от одного, то выходит соответствующее уведомление.
+    """
+    try:
+        child = Schoolkid.objects.get(full_name__contains = child)
+    except Schoolkid.DoesNotExist:
+        print('Записей не найдено')
+        return
+    except Schoolkid.MultipleObjectsReturned:
+        print('Найдено больше одной записи')
+        return
+    chastisement = Chastisement.objects.filter(schoolkid = child)
+    chastisement.delete()
+    return
+
+
+def create_commendation(child, subject):
+    """Создать поддельную запись с похвалой от учителя ученику.
+
+    Args:
+        child (str): ФИО ученика.
+        subject (str): Название предмета.
+
+    Returns:
+        На сайте появляется похвала от учителя.
+        Если количество найденных учеников отличается от одного, то выходит соответствующее уведомление.
+        Если название предмета указано неверно, то выходит соответствующее уведомление.
+    """
+    try:
+        child = Schoolkid.objects.get(full_name__contains = child)
+    except Schoolkid.DoesNotExist:
+        print('Записей не найдено')
+        return
+    except Schoolkid.MultipleObjectsReturned:
+        print('Найдено больше одной записи')
+        return
+    lesson_date = Lesson.objects.filter(year_of_study=child.year_of_study, group_letter=child.group_letter, subject__title=subject).last()
+    praise = [
+        'Молодец!',
+        'Отлично!',
+        'Хорошо!',
+        'Гораздо лучше, чем я ожидал!',
+        'Ты меня приятно удивил!',
+        'Великолепно!',
+        'Прекрасно!',
+        'Ты меня очень обрадовал!',
+        'Именно этого я давно ждал от тебя!',
+        'Сказано здорово – просто и ясно!',
+        'Ты, как всегда, точен!',
+        'Очень хороший ответ!',
+        'Талантливо!',
+        'Ты сегодня прыгнул выше головы!',
+        'Я поражен!',
+        'Уже существенно лучше!',
+        'Потрясающе!',
+        'Замечательно!',
+        'Прекрасное начало!',
+        'Так держать!',
+        'Ты на верном пути!',
+        'Здорово!',
+        'Это как раз то, что нужно!',
+        'Я тобой горжусь!',
+        'С каждым разом у тебя получается всё лучше!',
+        'Мы с тобой не зря поработали!',
+        'Я вижу, как ты стараешься!',
+        'Ты растешь над собой!',
+        'Ты многое сделал, я это вижу!',
+        'Теперь у тебя точно все получится!'
+    ]
+    text = choice(praise)
+    try:
+        Commendation.objects.create(text=text, created=lesson_date.date, schoolkid=child, subject=lesson_date.subject, teacher=lesson_date.teacher)
+    except AttributeError:
+        print('Название урока введено некорректно')
+        return
+    return
